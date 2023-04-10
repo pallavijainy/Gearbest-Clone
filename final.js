493be5641d64c862cea90169c5d2567d87032c22
Pallavi Jain <107476738+pallavijainy@users.noreply.github.com>
2023-04-10 16:17:23 +0530

    a

--- final.js
+++ final.js
@@ -1,23 +1,23 @@
 // collectionPage
 let arr_collection=JSON.parse(localStorage.getItem("collection")) || []
     let collection=[
-        {image:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16554/goods_thumb_220-v1/474893e6aae3.jpg",
+        {image:"https://images.unsplash.com/photo-1626247103090-b81889531015?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXRodW1ibmFpbHx8ODg4ODIxN3x8ZW58MHx8fHw%3D&dpr=1&auto=format&fit=crop&w=294&q=60",
          name:"Nimble Tech Store"
     },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/item/6862458881155723264/16534/goods_thumb_220-v3/1519af3adbf9.jpg",
+    image:"https://images.unsplash.com/photo-1598327105666-5b89351aff97?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNlbGwlMjBwaG9uZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
      name:"Laser Master"
 },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/item/6900746726869757952/16492/goods_thumb_220-v3/0331f75f74f0.jpg",
+    image:"https://images.unsplash.com/photo-1565849904461-04a58ad377e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8Y2VsbCUyMHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     name:"Hot xiomi Laptop"
 },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/store/6907043732781199360/16492/goods_thumb_220-v2/eb108f2fda03.jpg",
+    image:"https://images.unsplash.com/photo-1548378329-437e1ef34263?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fENvbnN1bWVyJTIwRWxlY3Ryb25pY3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     name:"TWOTREES Official Store"
 },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/store/6900399760277172224/16511/goods_thumb_220-v3/6b1417b9a752.jpg",
+    image:"https://images.unsplash.com/photo-1589492477829-5e65395b66cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fENvbnN1bWVyJTIwRWxlY3Ryb25pY3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
     name:"RUNFENGTE Official store"
 }
 ]
@@ -26,6 +26,8 @@ collection.forEach(function(el){
 let div=document.createElement("div");
 let image=document.createElement("img");
 image.setAttribute("src",el.image)
+
+
 
 let name=document.createElement("h4");
 name.innerText=el.name;
@@ -46,23 +48,23 @@ document.querySelector("#collection_container").append(div);
 // superDeal
 let arr_superdeal=JSON.parse(localStorage.getItem("super")) || []
     let superdeal=[
-        {image:"https://gloimg.gbtcdn.com/soa/gb/item/6878030535709618176/16526/goods_thumb_220-v1/3df76f2e19f0.jpg",
+        {image:"https://images.unsplash.com/photo-1588689115724-a624efec3c93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fENvbnN1bWVyJTIwRWxlY3Ryb25pY3N8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
          name:"Gaming wireless Headphones"
     },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16469/goods_thumb_220-v1/bf89aac60f4e.jpg",
+    image:"https://images.unsplash.com/photo-1598717873846-0418a74950f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Q29uc3VtZXIlMjBFbGVjdHJvbmljc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
      name:"xiomi blackstone electic"
 },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/item/6900399760277172224/16467/goods_thumb_220-v1/01fc7d134255.jpg",
+    image:"https://images.unsplash.com/photo-1547489401-fcada4966052?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Q29uc3VtZXIlMjBFbGVjdHJvbmljc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
     name:"New Bluetooth watch"
 },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/item/6899301501118902272/16551/goods_thumb_220-v3/6466b98d5354.jpg",
+    image:"https://images.pexels.com/photos/373638/pexels-photo-373638.jpeg?auto=compress&cs=tinysrgb&w=600",
     name:"Xiomi Aquara smart Temperature Humidity"
 },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/item/6878391336429613056/16469/goods_thumb_220-v1/66caa4e51717.jpg",
+    image:"https://images.pexels.com/photos/12021852/pexels-photo-12021852.jpeg?auto=compress&cs=tinysrgb&w=600",
     name:"xiomi ENCHEN boost electric Hair Trimmer"
 }
 ]
@@ -92,23 +94,23 @@ document.querySelector("#superdeal_container").append(div);
 
 let arr_pal=JSON.parse(localStorage.getItem("pal")) || []
 let pal=[
-        {image:"https://gloimg.gbtcdn.com/soa/gb/item/6878025592978206720/16570/goods_thumb_220-v3/3d88e72aa70b.jpg",
+        {image:"https://images.pexels.com/photos/11764413/pexels-photo-11764413.jpeg?auto=compress&cs=tinysrgb&w=600",
          name:"GPS Drone with "
     },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/item/6878030535709618176/16570/goods_thumb_220-v5/0399788174bd.jpg",
+    image:"https://images.pexels.com/photos/6023354/pexels-photo-6023354.jpeg?auto=compress&cs=tinysrgb&w=600",
      name:"camera"
 },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16571/goods_thumb_220-v1/5cd047a2fbea.jpg",
+    image:"https://images.pexels.com/photos/9842750/pexels-photo-9842750.jpeg?auto=compress&cs=tinysrgb&w=600",
     name:"xiomi phone"
 },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/store/6907043732781199360/16492/goods_thumb_220-v2/eb108f2fda03.jpg",
+    image:"https://images.pexels.com/photos/4171752/pexels-photo-4171752.jpeg?auto=compress&cs=tinysrgb&w=600g",
     name:"TWOTREES Official Store"
 },
 {
-    image:"https://gloimg.gbtcdn.com/soa/gb/store/6507287161137004544/16570/goods_thumb_220-v3/c71199a32b70.jpg",
+    image:"https://images.pexels.com/photos/1706694/pexels-photo-1706694.jpeg?auto=compress&cs=tinysrgb&w=600",
     name:"Mini Mosquito Machine"
 }
 ]
@@ -138,211 +140,211 @@ document.querySelector("#new_container").append(div);
 let arr_recommend=JSON.parse(localStorage.getItem("recommend")) || []
     let recommend=[
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16577/goods_img-v1/d94ff13e2a7b.jpg",
+            image:"https://images.pexels.com/photos/1029757/pexels-photo-1029757.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"360 G300H Dash Smart Car DVR 3MP Camera WIFI ",
             price:"79.00",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16575/goods_img-v1/77d59be42fb2.jpg",
+            image:"https://images.pexels.com/photos/2047905/pexels-photo-2047905.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Xiaomi Redmi Buds 4 Pro Earphone Bluetooth 3 Mic Wireless",
             price:"67.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6878399324569661440/16517/goods_img-v7/a00cb9f65d8f.jpg",
+            image:"https://images.pexels.com/photos/5412270/pexels-photo-5412270.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"TRONXY CRUX 1 3D printer Presale",
             price:"179.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16577/goods_img-v1/d94ff13e2a7b.jpg",
+            image:"https://images.pexels.com/photos/5412270/pexels-photo-5412270.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"M-855 Wireless Router Wifi 300M Signal",
             price:"14.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/store/6878025592978206720/16580/goods_img-v1/201adc2e44c0.jpg",
+            image:"https://images.pexels.com/photos/3992776/pexels-photo-3992776.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"JJRC H108 DIY Building Blocks Drone with HD",
             price:"36.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6878030535709618176/16577/goods_img-v1/688df4b94eb8.jpg",
+            image:"https://images.pexels.com/photos/3201768/pexels-photo-3201768.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"360 G300H DasInch  Sm We Maps Night Vision Parking Monitor",
             price:"38.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6862458881155723264/16545/goods_img-v3/a75d89f4a160.jpg",
+            image:"https://images.pexels.com/photos/2373730/pexels-photo-2373730.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"3HD 160 Degree Camera aps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6878400698325856256/16419/goods_img-v1/22549f61229b.jpg",
+            image:"https://images.pexels.com/photos/51396/pocket-watches-time-of-time-watches-51396.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Baseus GaN Power Bank USB Charger",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16560/goods_img-v1/7dd97992ee49.jpg",
+            image:"https://images.pexels.com/photos/1162519/pexels-photo-1162519.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Headphones with Wireless Stereo",
             price:"549.99",
             flash:"Flash Sale"
         },
          {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6862458881155723264/16534/goods_img-v3/1519af3adbf9.jpg",
+            image:"https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"FHD 160 Degree MP Camera WIFI GPS Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16563/goods_img-v1/19e5bb35066f.jpg",
+            image:"https://images.unsplash.com/photo-1529111424977-59e5afaf5824?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3w4Mzc0OTExfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
             name:"V9 Wireless Bluetooth CSR V4.1 Headset ",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16554/goods_img-v1/474893e6aae3.jpg",
+            image:"https://images.unsplash.com/photo-1455894127589-22f75500213a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTB8ODM3NDkxMXx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60",
             name:"Headphones with Wireless Stereo Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16465/goods_img-v1/8fc8853ff3b3.jpg",
+            image:"https://images.unsplash.com/photo-1542124292-60272943a355?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHw4Mzc0OTExfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
             name:"V9 Wireless Bluetooth CSR V4.1",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16538/goods_img-v1/1d662f23ff41.jpg",
+            image:"https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Camera Professional Brushless Toys -",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6900386065325158400/16545/goods_img-v1/7fb1c3c77ea9.jpg",
+            image:"https://images.pexels.com/photos/14434416/pexels-photo-14434416.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"360 G300H Dash Cam 1296P FHD 160 Degree 2.0 Inch  Smart Car DVR 3MP Camera WIFI GPS Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6908377430696849408/16521/goods_img-v1/7411a7090935.jpg",
+            image:"https://images.pexels.com/photos/10433477/pexels-photo-10433477.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Men Sport Shoes Sneakers Sneaker 4 4th Men Running Lightweight",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/16061/goods_img-v1/51299ec84910.jpg",
+            image:"https://images.pexels.com/photos/6370370/pexels-photo-6370370.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Men Sport Shoes Sneakers Sneaker 4 4th Men Running Lightweight",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16467/goods_img-v2/7f3ed3c839fe.jpg",
+            image:"https://images.pexels.com/photos/11044768/pexels-photo-11044768.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Men Sport Shoes Sneakers Sneaker 4 4th Men Running Lightweight",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6878402251153666048/16514/goods_img-v9/286a149e4bd4.jpg",
+            image:"https://images.pexels.com/photos/12985958/pexels-photo-12985958.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:" DVR 3MP Camera WIFI GPS Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/15928/goods_img-v4/accae4e3d6a5.jpg",
+            image:"https://images.pexels.com/photos/12410982/pexels-photo-12410982.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"amera WIFI GPS Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6898341271098814464/16575/goods_img-v1/c61e7f592465.jpg",
+            image:"https://images.pexels.com/photos/10933699/pexels-photo-10933699.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:" Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/store/6878025592978206720/16531/goods_img-v1/6cd7f3cabfaf.jpg",
+            image:"https://images.pexels.com/photos/4417768/pexels-photo-4417768.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:" Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6878024886355423232/16453/goods_img-v2/677443d1848e.jpg",
+            image:"https://images.pexels.com/photos/6791447/pexels-photo-6791447.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Men Sport Shoes Sneakers Sneaker 4 4th Men Running Lightweight",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/store/6866331803104505856/16421/goods_img-v3/d6e1acb6230c.jpg",
+            image:"https://images.pexels.com/photos/3892900/pexels-photo-3892900.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:" Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/store/6615272824267153408/16427/goods_img-v3/093e9bc5ff1f.jpg",
+            image:"https://images.pexels.com/photos/3738406/pexels-photo-3738406.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"GPS Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6866325577285169152/16388/goods_img-v1/f67c07d453c0.jpg",
+            image:"https://images.pexels.com/photos/4089484/pexels-photo-4089484.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Car DVR 3MP Camera WIFI GPS Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6870745450383863808/16434/goods_img-v1/5690c2eeed7d.jpg",
+            image:"https://images.pexels.com/photos/4089473/pexels-photo-4089473.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Smart Car DVR 3MP Camera WIFI GPS Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/15960/goods_img-v1/694fe0be3aed.jpg",
+            image:"https://images.pexels.com/photos/4158/apple-iphone-smartphone-desk.jpg?auto=compress&cs=tinysrgb&w=600",
             name:" Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6878399324569661440/16576/goods_img-v4/f1105fe72ae0.jpg",
+            image:"https://images.pexels.com/photos/4065617/pexels-photo-4065617.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"360 G300H Dash Cam 1296P FHD 160 Degree 2.0 Inch  Smart Car ",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6866331803104505856/16390/goods_img-v1/351c2e265cf6.jpg",
+            image:"https://images.unsplash.com/photo-1542124292-60272943a355?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHw4Mzc0OTExfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
             name:"360 G300H Dash Cam 1296P FHD 160",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6866331933228593152/16538/goods_img-v1/75ce5cfbb946.jpg",
+            image:"https://images.pexels.com/photos/205926/pexels-photo-205926.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/16012/goods_img-v1/46d319b41f27.jpg",
+            image:"https://images.pexels.com/photos/3802602/pexels-photo-3802602.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"630 G300H Dash Cam 1296P FHD 160 Degree 2.0 Inch  Smart Car ",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/15959/goods_img-v2/1338948eb469.jpg",
+            image:"https://images.pexels.com/photos/5741878/pexels-photo-5741878.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:" Camera WIFI GPS Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/store/6507287161137004544/16402/goods_img-v3/cf56ccf1c1ea.jpg",
+            image:"https://images.pexels.com/photos/185030/pexels-photo-185030.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:" Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
         },
         {
-            image:"https://gloimg.gbtcdn.com/soa/gb/item/6615272824267153408/15967/goods_img-v1/06294f8420ed.jpg",
+            image:"https://images.pexels.com/photos/164710/pexels-photo-164710.jpeg?auto=compress&cs=tinysrgb&w=600",
             name:"Smart Car DVR 3MP Camera WIFI GPS Google Maps Night Vision Parking Monitor",
             price:"549.99",
             flash:"Flash Sale"
